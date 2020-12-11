848bf9a039c96aee7ca904c23d609e9b81023d3b
Randel M <Randel.C.Moore@gmail.com>
2020-12-11 08:23:36 -0800

    initialize project

---
+++ Bakery/Models/ClassName.cs
@@ -0,0 +1,16 @@
+using System;
+//using System.Collections.Generic; -- use for lists
+
+namespace Bakery.Models
+{
+  public class Bread
+  {
+    //fields are hidden
+    public vartype FieldName { get; set; } //this is a property
+
+    //consturctor
+    //other methods
+  }
+
+  public class Pastry
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System;
+
+namespace BakeryUI
+{
+
+  public class Program
+  {
+    public static void Main()
+    {
+      
+    }
+  }
+}
