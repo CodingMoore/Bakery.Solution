be0fbe86f5bf98dc711f23518dd053d3da24aada
Randel M <Randel.C.Moore@gmail.com>
2020-12-11 11:56:06 -0800

    add some basic UI logic

--- Program.cs
+++ Program.cs
@@ -8,7 +8,44 @@ namespace BakeryUI
   {
     public static void Main()
     {
-      WriteLine
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Order();
+
+      // if (Cart is empty);
+      // {
+      //   Console.WriteLine("Your shopping cart is empty.");
+      // }
+      // else 
+      // {
+      //   CartReport();
+      // }
+    }  
+    
+    public static Order()
+    {
+     
+      Console.WriteLine("To add Bread to your cart, please type 'Bread'.");
+      Console.WriteLine("To add Pastry to your cart, please Type 'Pastry'.");
+      Console.WriteLine("To puchase the items in your cart, please type 'Purchase'.");
+      Console.WriteLine("To empty your cart, please type 'Restart'.");
+      CartReport();
+      string custInput = Console.ReadLine();
+    } 
+    
+
+    public static CartReport()
+    {
+       if (Cart is empty)
+      {
+        Console.WriteLine("Your shopping cart is empty.");
+      }
+      else 
+      {
+      Console.WriteLine("Your shopping cart contains...");
+      Console.WriteLine(Cart contents);
+      Console.WriteLine("Your total comes to...");
+      Console.WriteLine(Total Cost);
+      }
     }
   }
 }
