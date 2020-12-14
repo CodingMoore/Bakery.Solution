1d776adbe73676357275bce078617d2417ccc902
Randel M <Randel.C.Moore@gmail.com>
2020-12-13 17:19:58 -0800

    return project to a working state in main branch

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,17 +1,40 @@
+using System.Collections.Generic;
 using System;
 //using System.Collections.Generic; -- use for lists
 
-namespace Bread.Models
+namespace Bakery.Models
 {
   public class Bread
   {
     //fields are hidden
-    private static List<I
+    private static List<Bread> _breadCart = new List<Bread> {};
     public string Name { get; set; } //this is a property
     public int Price { get; set; }
 
+    public Bread(string name)
+    {
+      Name = name;
+      Price = 5;
+      _breadCart.Add(this);
+    }
 
-    //consturctor
-    //other methods
+    public static List<Bread> GetBread()
+    {
+      return _breadCart;
+    }
+
+    public static void ClearBreadCart()
+    {
+      _breadCart.Clear();
+    }
+
+    public static void DisplayBreadList()
+    {
+      foreach (Bread thingy in Bread._breadCart)
+      {
+        Console.Write(thingy.Name + " " + "$" + thingy.Price + ", ");
+      }
+    }
   }
 }
+
