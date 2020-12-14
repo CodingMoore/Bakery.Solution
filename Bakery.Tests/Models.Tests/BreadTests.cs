3827dede71e95b268fdf9279fd3c39405e5f646f
Randel M <Randel.C.Moore@gmail.com>
2020-12-13 18:06:46 -0800

    comment out unneeded tests

--- Bakery.Tests/Models.Tests/BreadTests.cs
+++ Bakery.Tests/Models.Tests/BreadTests.cs
@@ -19,37 +19,38 @@ namespace Bakery.Tests
     //   Console.WriteLine(Bread.GetBread()[0].Name);
     //   Assert.AreEqual("rye", "ry");
     // }
-    // public void BreadConstructor_InstantiateBreadObject_True()
-    // {
-    //   // any necessary logic to prep for test; instantiating new classes, etc.
-    //   Bread loaf1 = new Bread("rye", 3);
-    //   Assert.AreEqual(typeof(Bread), loaf1.GetType());
-    // }
 
-    // [TestMethod]
-    // public void BreadConstructor_AddToStaticList_True()
-    // {
-    //   Bread loaf1 = new Bread("testInput", 3);
-    //   Assert.AreEqual(loaf1.GetType(), Bread.GetBread()[0].GetType());
+    [TestMethod]
+    public void BreadConstructor_InstantiateBreadObject_True()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Bread loaf1 = new Bread("rye", 3);
+      Assert.AreEqual(typeof(Bread), loaf1.GetType());
+    }
+
+    [TestMethod]
+    public void BreadConstructor_AddToStaticList_True()
+    {
+      Bread loaf1 = new Bread("testInput", 3);
+      Assert.AreEqual(loaf1.GetType(), Bread.GetBread()[0].GetType());
       
-    // }
+    }
 
-    // [TestMethod]
+    [TestMethod]
+    public void ClearBreadCart_EmptyTheList_True()
+    {
+      Bread loaf1 = new Bread("testInput", 3);
+      Assert.AreEqual(1, Bread.GetBread().Count);
+    }
 
-    // public void ClearBreadCart_EmptyTheList_True()
-    // {
-    //   Bread loaf1 = new Bread("testInput", 3);
-    //   Assert.AreEqual(1, Bread.GetBread().Count);
-    // }
-
-    // [TestMethod]
-    // public void DisplayBreadList_ShowListContents_True()
-    // {
-    //   Bread loaf1 = new Bread("Bread Name1", 3);
-    //   Bread.DisplayBreadList();
-    //   Assert.AreEqual("Bread Name1", Bread.GetBread()[0].Name);
-    //   Assert.AreEqual(5, Bread.GetBread()[0].Price);
-    // }
+    [TestMethod]
+    public void DisplayBreadList_ShowListContents_True()
+    {
+      Bread loaf1 = new Bread("Bread Name1", 3);
+      Bread.DisplayBreadList();
+      Assert.AreEqual("Bread Name1", Bread.GetBread()[0].Name);
+      Assert.AreEqual(5, Bread.GetBread()[0].Price);
+    }
 
     [TestMethod]
     public void UpdateBread_ChangeListObjectValues_True()
@@ -61,4 +62,3 @@ namespace Bakery.Tests
     }
   }
 } 
-//  Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
