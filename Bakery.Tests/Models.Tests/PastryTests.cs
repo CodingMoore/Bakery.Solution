adc62240af4643814d727fd00917d422bb3ebc8c
Randel M <Randel.C.Moore@gmail.com>
2020-12-13 20:48:37 -0800

    pass PastryPrice() test

--- Bakery.Tests/Models.Tests/PastryTests.cs
+++ Bakery.Tests/Models.Tests/PastryTests.cs
@@ -53,7 +53,7 @@ namespace Bakery.Tests
       Pastry.GetPastry()[0].Quantity = 3;
       Pastry.GetPastry()[1].Quantity = 3;
       Pastry.GetPastry()[2].Quantity = 1;
-      Assert.AreEqual(10, Pastry.PastryPrice());
+      Assert.AreEqual(12, Pastry.PastryPrice());
     }
   }
 } 
